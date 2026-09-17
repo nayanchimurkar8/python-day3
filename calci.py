5a88c13310dcdd2a43feb376ba7ce225d38ecc9c
Nayan Chimurkar <nayanchimurkar8@gmail.com>
2026-09-17 14:35:47 +0530

    py

---
+++ calci.py
@@ -0,0 +1,20 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+
+def add(a,b):
+    return a+b
+print(add(a,b))
+
+def sub(a,b):
+    return a-b
+print(sub(a,b))
+
+def mul(a,b):
+    return a*b
+print(mul(a,b))
+
+def div(a,b):
+    return a/b
+print(div(a,b))
+
+
